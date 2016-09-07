a9b09b0a087bd53d55c329e52f5fbf73c1da55d9
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-07 18:23:42 +1000

    Model fill and 1st test add

--- hero.rb
+++ hero.rb
@@ -1,3 +1,34 @@
 class Hero
+  def initialize (name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def name
+    @name
+  end
+
+  def power_up
+    @health += 10
+  end
+
+  def power_down
+    @health -= 10
+  end
+
+  def hero_info
+    "#{@name} has #{@health} health"
+  end
 
 end
+
+# hero = Hero.new 'yoda'
+
+# puts hero.name
+# puts hero.hero_info
+# hero.power_down
+# puts hero.hero_info
+# hero.power_down
+# hero.power_down
+# hero.power_up
+# hero.hero_info
--- hero_spec.rb
+++ hero_spec.rb
@@ -1,5 +1,9 @@
 require './hero'
 describe Hero do 
+  it "has a capitalized name" do
+    hero = Hero.new 'kenobi'
 
+    expect(hero.name).to eq 'Kenobi'
+  end
   
 end
