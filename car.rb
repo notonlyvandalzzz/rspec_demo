3cfca8a45f03ba1166a4b682c5c77576c520c8e2
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-08 11:50:11 +1000

    Car model and test add

---
+++ car.rb
@@ -0,0 +1,27 @@
+class Car
+  MPG = 20
+
+  attr_reader :fuel
+
+  def initialize
+    @fuel = 0
+  end
+
+  def add_fuel gal
+    @fuel += gal
+  end
+
+  def range
+    0
+  end
+
+  def fuel_left
+    @fuel
+  end
+
+
+end
+
+car = Car.new
+car.add_fuel 10
+puts "Range is #{car.range}"
