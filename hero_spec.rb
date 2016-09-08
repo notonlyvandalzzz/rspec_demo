75419e14052437d955cb397b871115621a23cf22
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-09-08 10:05:18 +1000

    small refactoring

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,27 +1,32 @@
 require './hero'
 describe Hero do 
-  it "has a capitalized name" do
-    hero = Hero.new 'kenobi'
 
-    expect(hero.name).to eq 'Kenobi'
+  before do
+    @hero = Hero.new 'kenobi'
+
+  end
+  it "has a capitalized name" do
+    # hero = Hero.new 'kenobi'
+
+    expect(@hero.name).to eq 'Kenobi'
   end
 
   it "can power up" do
-    hero = Hero.new 'kenobi'
+    # hero = Hero.new 'kenobi'
 
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
     it "can power down" do
-    hero = Hero.new 'kenobi'
+    # hero = Hero.new 'kenobi'
 
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
   
   it "fuul info" do
-    hero = Hero.new 'kenobi'
+    # hero = Hero.new 'kenobi'
 
-    expect(hero.hero_info).to eq 'Kenobi has 100 health'
+    expect(@hero.hero_info).to eq 'Kenobi has 100 health'
   end
 
 end
